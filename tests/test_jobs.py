ac9c75be8935863e0a83098b1fdefe4c277c7fb6
Ilya Gruzinov <igruzinov@yandex.ru>
2019-07-15 16:12:50 +0300

    Fix new job status/description handling

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -39,7 +39,8 @@ async def test_unschedulable_job_lifecycle(helper: Helper) -> None:
 
     assert job.history.reason == "ClusterScaleUpFailed"
     assert (
-        job.history.description == "Cannot scaleup the cluster to get more resources."
+        job.history.description
+        == "Failed to scale up the cluster to get more resources"
     )
     # Check that it is not in a running job list anymore
     jobs = await helper.client.jobs.list(
